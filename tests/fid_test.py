9ef355889243e0d3109c9f88786998ec07d88e0a
davesean <haldimann.d@gmail.com>
2019-01-30 10:44:36 +0100

    Added arg of n and compute FID for training on n randomly chosen samples.

--- tests/fid_test.py
+++ tests/fid_test.py
@@ -7,19 +7,35 @@ import fid
 from scipy.misc import imread
 import tensorflow as tf
 import cv2
+import argparse
+import random
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--path", help="path to folder with images")
+a = parser.parse_args()
+
 inception_path = fid.check_path_inception("/Users/David/Downloads/inception-2015-12-05") # download inception network
 
 fid.create_inception_graph(inception_path)  # load the graph into the current TF graph
 
 def get_images(path,prefix):
     paths = glob.glob(os.path.join(path,prefix+"*.png"))
-    num_samples = len(paths)
-    images = np.zeros((num_samples,256,256,3))
+    if prefix.split('_')[-1] == training:
+        num_samples = 500
+
+        random.seed(42)
+        paths = random.sample(paths,num_samples)
+        images = np.zeros((num_samples,256,256,3))
+    else:
+        num_samples = len(paths)
+        images = np.zeros((num_samples,256,256,3))
+
+
 
     for i,path in enumerate(paths):
         images[i,:,:,:]= cv2.imread(path)[...,::-1]
     return images
-base_path = "/Users/David/masterThesis/pix2pix-tensorflow/dir/264_full/validation"
+base_path = a.path
 num = base_path.split('/')[-2].split('_')[0]
 ending = base_path.split('/')[-1]
 set_real = get_images(base_path, num+"_"+ending)
@@ -33,4 +49,4 @@ with tf.Session() as sess:
     mu_gen, sigma_gen = fid.calculate_activation_statistics(set_fake, sess, batch_size=set_fake.shape[0])
 
 fid_value = fid.calculate_frechet_distance(mu_gen, sigma_gen, mu_real, sigma_real)
-print("FID: %s" % fid_value)
+print("FID for %s set of run %s: %s" % (ending,num,fid_value))
